7ce11d29ad8d2428f92b8ac1cf154a1091536891
Mauro Baptista <mauro@carnou.com>
2020-08-01 13:14:28 -0300

    Added needed models

---
+++ database/factories/EndpointFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Endpoint;
+use Faker\Generator as Faker;
+
+$factory->define(Endpoint::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
