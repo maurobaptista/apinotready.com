b4b20d32792f73d601b5f1e32736318e116084df
Mauro Baptista <mauro@carnou.com>
2020-08-03 18:53:23 -0300

    Simplified form

--- resources/views/components/input.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="mb-4">
-    <label for="{{ $name }}">{{ ucfirst($name) }}</label>
-    <input wire:model="{{ $name }}" name="{{ $name }}" type="{{ $type ?? 'text' }}" class="input @error((string) $name) input-error @enderror">
-    @error((string) $name)
-        <span class="error">{{ $message }}</span>
-    @enderror
-</div>
--- resources/views/livewire/endpoint/create.blade.php
+++ resources/views/livewire/endpoint/create.blade.php
@@ -1,33 +1,52 @@
 <div>
     <form wire:submit.prevent="submit" class="bg-gray-300 p-2">
-        @component('components.input')
-            @slot ('name') email @endslot
-            @slot ('type') email @endslot
-        @endcomponent
+        <div class="mb-4">
+            <label for="email">Email</label>
+            <input wire:model="email" name="email" type="email" class="input @error('email') input-error @enderror">
+            @error('email')
+                <span class="error">{{ $message }}</span>
+            @enderror
+        </div>
 
-        @component('components.input')
-            @slot ('name') endpoint @endslot
-        @endcomponent
+        <div class="mb-4">
+            <label for="endpoint">Endpoint</label>
+            <input wire:model="endpoint" name="endpoint" type="text" class="input @error('endpoint') input-error @enderror">
+            @error('endpoint')
+                <span class="error">{{ $message }}</span>
+            @enderror
+        </div>
 
-        @component('components.select', [
-            'options' => collect($methods)
-                ->mapWithKeys(function (string $value) { return [$value => $value];}),
-        ])
-            @slot ('name') method @endslot
-        @endcomponent
+        <div class="mb-4">
+            <label for="method">Method</label>
+            <select wire:model="method" name="method" class="input @error('method') input-error @enderror">
+                @foreach ($methods as $method)
+                    <option value="{{ $method }}">{{ $method }}</option>
+                @endforeach
+            </select>
+            @error('method')
+                <span class="error">{{ $message }}</span>
+            @enderror
+        </div>
 
-        @component('components.select', [
-            'options' => $responses,
-            'display' => function ($key, $value) {
-                return "[$key] $value";
-            },
-        ])
-            @slot ('name') response @endslot
-        @endcomponent
+        <div class="mb-4">
+            <label for="response">Response</label>
+            <select wire:model="response" name="response" class="input @error('response') input-error @enderror">
+                @foreach ($responses as $responseCode => $responseDescription)
+                    <option value="{{ $responseCode }}">[{{ $responseCode }}] {{ $responseDescription }}</option>
+                @endforeach
+            </select>
+            @error('response')
+                <span class="error">{{ $message }}</span>
+            @enderror
+        </div>
 
-        @component('components.textarea')
-            @slot ('name') body @endslot
-        @endcomponent
+        <div class="mb-4">
+            <label for="body">Body</label>
+            <textarea wire:model="body" name="body" rows="4" class="input @error('body') input-error @enderror"></textarea>
+            @error('body')
+                <span class="error">{{ $message }}</span>
+            @enderror
+        </div>
 
         <button type="submit" class="btn">
             Save Endpoint
