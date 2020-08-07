6c6c1a142e2ceb23dadfd1dbbbe50cf989d031c2
Mauro Baptista <mauro@carnou.com>
2020-08-07 15:57:21 -0300

    Improve layout - not much tough :)

--- resources/views/livewire/endpoint/create.blade.php
+++ resources/views/livewire/endpoint/create.blade.php
@@ -1,52 +1,79 @@
 <div>
-    <form wire:submit.prevent="submit" class="bg-gray-300 p-2">
+    <form wire:submit.prevent="store" class="bg-gray-300 p-2">
         <div class="mb-4">
-            <label for="email">Email</label>
+            <label for="email">Email (not required)</label>
             <input wire:model="email" name="email" type="email" class="input @error('email') input-error @enderror">
             @error('email')
                 <span class="error">{{ $message }}</span>
             @enderror
         </div>
 
-        <div class="mb-4">
-            <label for="endpoint">Endpoint</label>
-            <input wire:model="endpoint" name="endpoint" type="text" class="input @error('endpoint') input-error @enderror">
-            @error('endpoint')
-                <span class="error">{{ $message }}</span>
-            @enderror
-        </div>
-
-        <div class="mb-4">
-            <label for="method">Method</label>
-            <select wire:model="method" name="method" class="input @error('method') input-error @enderror">
+        <div class="mb-4 inline-flex">
+            <select wire:model="method" name="method"
+                    class="
+                        flex flex-1
+                        rounded-l-md
+                        bg-gray-400
+                        p-3
+                        @error('method') input-error @enderror
+                    "
+            >
                 @foreach ($methods as $method)
                     <option value="{{ $method }}">{{ $method }}</option>
                 @endforeach
             </select>
-            @error('method')
-                <span class="error">{{ $message }}</span>
-            @enderror
+
+            <input wire:model="segments" name="segments" type="text"
+                   class="
+                        flex-grow-1 w-full
+                        rounded-r-md
+                        bg-gray-100
+                        p-3
+                        @error('endpoint') input-error @enderror
+                   "
+            >
         </div>
 
-        <div class="mb-4">
+        <div class="mb-4 relative">
             <label for="response">Response</label>
-            <select wire:model="response" name="response" class="input @error('response') input-error @enderror">
+            <select wire:model="response" name="response"
+                    class="
+                        absolute
+                        right-0
+                        w-16
+                        @error('response') input-error @enderror
+                    ">
                 @foreach ($responses as $responseCode => $responseDescription)
                     <option value="{{ $responseCode }}">[{{ $responseCode }}] {{ $responseDescription }}</option>
                 @endforeach
             </select>
-            @error('response')
-                <span class="error">{{ $message }}</span>
-            @enderror
+
+            <textarea wire:model="body" name="body" rows="4"
+                      class="
+                          w-full
+                          rounded-md
+                          @error('body') input-error @enderror
+                      ">
+            </textarea>
+
         </div>
 
-        <div class="mb-4">
-            <label for="body">Body</label>
-            <textarea wire:model="body" name="body" rows="4" class="input @error('body') input-error @enderror"></textarea>
-            @error('body')
-                <span class="error">{{ $message }}</span>
-            @enderror
-        </div>
+        @error('method')
+            <span class="error">{{ $message }}</span>
+        @enderror
+
+        @error('segments')
+            <span class="error">{{ $message }}</span>
+        @enderror
+
+        @error('response')
+            <span class="error">{{ $message }}</span>
+        @enderror
+
+        @error('body')
+            <span class="error">{{ $message }}</span>
+        @enderror
+
 
         <button type="submit" class="btn">
             Save Endpoint
@@ -54,8 +81,10 @@
     </form>
 
     @if ($recentCreatedEndpoint)
-        <div >
+        <div>
             Endpoint Created
         </div>
     @endif
+
+
 </div>
