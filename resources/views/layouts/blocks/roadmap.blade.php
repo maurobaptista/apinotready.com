aab93cd470bf5b5dd77cc1f1148434eb96f54077
Mauro Baptista <mauro@carnou.com>
2020-08-21 09:12:33 -0300

    Updated tailwind and organized layout

---
+++ resources/views/layouts/blocks/roadmap.blade.php
@@ -0,0 +1,16 @@
+<div class="bg-white">
+    <div class="w-full m-auto max-w-xs md:max-w-2xl p-10">
+        <h2 class="text-gradient text-4xl font-title font-bold md:absolute">
+            Roadmap
+        </h2>
+        <article class="mt-4 text-lg mt-16">
+            <p class="mb-6">Ideas for a near future:</p>
+            <ul class="list-disc list-inside pl-4">
+                <li>Improve layout</li>
+                <li>No password login</li>
+                <li>Endpoint management</li>
+                <li>Easy switch responses per endpoint</li>
+            </ul>
+        </article>
+    </div>
+</div>
